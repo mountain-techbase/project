2d271aa59147b765f0f79df4b137cf7f6ea5328e
soshiito <52130945+soshiito@users.noreply.github.com>
2019-08-13 21:51:42 +0900

    Update mission7_new.php

--- mission7_new.php
+++ mission7_new.php
@@ -1,61 +1,83 @@
+
+<?php
+session_start();
+
+header("Content-type: text/html; charset=utf-8");
+
+//クロスサイトリクエストフォージェリ（CSRF）対策
+$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+$token = $_SESSION['token'];
+
+//クリックジャッキング対策
+header('X-FRAME-OPTIONS: SAMEORIGIN');
+
+?>
 <html>
-	<head>
-		<title>アカウント新規作成</title>
-		<link rel="stylesheet" type="text/css" href="mission7_style.css" />
-	</head>
-	<body>
-		<h1>ゴミ箱情報.com(仮称)</h1>
-		<table>
-			<tr>
-				<td class="navi1">
-					<a href="mission7_new.php">新規作成</a>
-				</td>
-				<td class="navi1">
-					<a href="mission7_log.php">ログイン・ログアウト</a>
-				</td>
-			</tr>
-		</table>
+    <head>
+        <title>アカウント新規作成</title>
+        <link rel="stylesheet" type="text/css" href="mission7_style.css" />
+    </head>
+    <body>
+        <h1>ゴミ箱情報.com(仮称)</h1>
+        <table>
+            <tr>
+                <td class="navi1">
+                    <a href="mission7_new.php">新規作成</a>
+                </td>
+                <td class="navi1">
+                    <a href="mission7_log.php">ログイン・ログアウト</a>
+                </td>
+            </tr>
+        </table>
 
-		<table border>
-			<tr>
-				<td class="navi2">
-					<a href="mission7_index.php">ホーム</a>
-				</td>
-				<td class="navi2">
-					<a href="mission7_mypage.php">マイページ</a>
-				</td>
-				<td class="navi2">
-					<a href="mission7_review.php">投稿</a>
-				</td>
-			</tr>
-		</table>
-		<br />
-		<h2>アカウント新規作成</h2>
+        <table border>
+            <tr>
+                <td class="navi2">
+                    <a href="mission7_index.php">ホーム</a>
+                </td>
+                <td class="navi2">
+                    <a href="mission7_mypage.php">マイページ</a>
+                </td>
+                <td class="navi2">
+                    <a href="mission7_review.php">投稿</a>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <h2>アカウント新規作成</h2>
+        <form action="registration_mail_check.php" method="post">
 
-		<hr />
+            <p>メールアドレス：<input type="text" name="mail" size="50"></p>
 
-	</body>
-	<footer>
-	<p id="sitemap">サイトマップ</p>
-		<ul>
-			<li>
-				<a href="mission7_index.php">ホーム</a>
-			</li>
-			<li>
-				<a href="mission7_mypage.php">マイページ</a>
-			</li>
-			<li>
-				<a href="mission7_review.php">投稿</a>
-			</li>
-			<li>
-				<a href="mission7_help.php">ヘルプ</a>
-			</li>
-			<li>
-				<a href="mission7_origin.php">このサイトについて</a>
-			</li>
-			<li>
-				<a href="mission7_admin.php">管理者用ページ</a>
-			</li>
-		</ul>
-	</footer>
+            <input type="hidden" name="token" value="<?=$token?>">
+            <input type="submit" value="登録する">
+
+        </form>
+
+        <hr />
+
+    </body>
+    <footer>
+    <p id="sitemap">サイトマップ</p>
+        <ul>
+            <li>
+                <a href="mission7_index.php">ホーム</a>
+            </li>
+            <li>
+                <a href="mission7_mypage.php">マイページ</a>
+            </li>
+            <li>
+                <a href="mission7_review.php">投稿</a>
+            </li>
+            <li>
+                <a href="mission7_help.php">ヘルプ</a>
+            </li>
+            <li>
+                <a href="mission7_origin.php">このサイトについて</a>
+            </li>
+            <li>
+                <a href="mission7_admin.php">管理者用ページ</a>
+            </li>
+        </ul>
+    </footer>
 </html>
